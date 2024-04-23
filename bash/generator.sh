e13f4706012a041c7a6044f00bf209b184b12431
hao.cheong <hao.cheong@kgit.com.au>
2024-04-23 16:30:23 +1000

    started model generating

--- bash/generator.sh
+++ bash/generator.sh
@@ -47,7 +47,52 @@ function generate_base_files() {
 function generate_models() {
     echo "======== GENERATE MODELS FILES ========"
 
-    # For every instance 
+    # For every instance
+    for model in $(jq -r '.tables[] | .name ' config.json)
+    do
+        echo "> Generating model for $model"
+        model_lc=$(echo "$model" | tr '[:upper:]' '[:lower:]')
+
+        # Create default template
+        model_template=$(cat ./templates/model_templates.txt | grep -e "<<MODEL_BASE>>" -A16 | tail -15)
+        echo "$model_template" > ./project/app/models/"$model_lc"_model.py
+
+
+
+        # Create get all the column
+        model_cols=$(jq -r ' .tables[] | select(.name == "'$model'") | .columns[] | [.column_name, .column_type] | join(",") ' config.json)
+
+        for cols in $model_cols
+        do
+            # echo $cols
+            model_col_name=$(echo $cols | cut -d"," -f1)
+            model_col_type=$(echo $cols | cut -d"," -f2)
+
+            echo $model_col_name
+            echo $model_col_type
+
+            if [[ $model_col_type == 'str' ]]
+            then
+                model_column_str=$(cat ./templates/model_templates.txt | grep -e "<<COLUMN_STRING>>" -A2 | tail -2)
+                filled_model_column_str=$(echo "$model_column_str" | sed -r "s/\{\{ COLUMN_NAME \}\}/$model_col_name/g")
+                
+                echo "$filled_model_column_str"
+
+                # new_file=$(cat ./project/app/models/"$model_lc"_model.py | sed -r "s/\{\{ COLUMNS \}\}/$filled_model_column_str/g")
+                cat ./project/app/models/"$model_lc"_model.py | sed -r "s/COLUMNS/$filled_model_column_str/g" 
+                
+            elif [[ $model_col_type == 'int' ]]
+            then
+                model_column_int=$(cat ./templates/model_templates.txt | grep -e "<<COLUMN_INTEGER>>" -A2 | tail -2)
+                filled_model_column_int=$(echo "$model_column_int" | sed -r "s/\{\{ COLUMN_NAME \}\}/$model_col_name/g")
+                echo "$filled_model_column_int"
+
+                
+                # echo $new_file > ./project/app/models/"$model_lc"_model.py
+            fi
+        done
+    done
+
 }
 # Start with model
 
@@ -63,3 +108,4 @@ fi
 # BASE GEN
 generate_base_directories
 generate_base_files
+generate_models
