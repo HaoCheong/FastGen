4627c3c1b86f3da2a48d08bf834537a0d87ef710
hao.cheong <hao.cheong@kgit.com.au>
2024-06-05 09:19:18 +1000

    Removed debug messages

--- generators/helpers.sh
+++ generators/helpers.sh
@@ -55,9 +55,6 @@ function validate_rel() {
     do
         max_count=$(jq -r ' .tables[] | select(.name == "'$table'") |  .columns[] | .column_name ' $CONFIG_NAME | sort | uniq -c | cut -d" " -f7 | sort | uniq)
         max_count_line=$(echo "$max_count" | wc -l)
-        echo $table
-        echo "MAX $max_count"
-        echo "MAX COUNT LINE $max_count_line"
         if [[ $max_count_line -gt 1 ]] || [[ $max_count -gt 1 ]]; then
             echo "Duplicate column name in table $table"
             exit
